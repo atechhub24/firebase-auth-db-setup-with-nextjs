fa30765d4502fc877539fed95f3dbb7e73fc2f19
atechhub24 <atechhub24.7@gmail.com>
2025-10-13 19:59:22 +0530

    Wrap children and Toaster components with FirebaseProvider in RootLayout for improved authentication context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { FirebaseProvider } from "@/components/providers/firebase-provider";
 import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -28,8 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <Toaster />
+        <FirebaseProvider>
+          {children}
+          <Toaster />
+        </FirebaseProvider>
       </body>
     </html>
   );
