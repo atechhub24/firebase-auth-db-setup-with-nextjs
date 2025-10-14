bb5e8e838b674cb5fbe5fc7d8d5b741c869cdcfb
atechhub24 <atechhub24.7@gmail.com>
2025-10-14 08:12:21 +0530

    Add RoleLayout component to structure role-based views
    - Introduced RoleLayout component to encapsulate the layout for role-specific pages.
    - Integrated AppHeader and AppSidebar components for consistent navigation and header display.
    - Utilized SidebarProvider and SidebarInset for improved layout management and responsiveness.

---
+++ app/[role]/layout.tsx
@@ -0,0 +1,23 @@
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { AppHeader } from "@/components/layout/app-header";
+import { AppSidebar } from "@/components/layout/app-sidebar";
+
+export default function RoleLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <AppHeader />
+          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+            <main className="flex-1">{children}</main>
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    </>
+  );
+}
